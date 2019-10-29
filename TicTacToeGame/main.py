1c7735361d4eccd643cfa24289ced62fae504d0c
DeeaMG <deeaganea@gmail.com>
2019-10-30 01:16:01 +0200

    Create main.py

---
+++ TicTacToeGame/main.py
@@ -0,0 +1,87 @@
+class PlayGame:
+
+    def __init__(self):
+        self.matrice = [
+            [ 'O', 'O', 'O' ],
+            [ 'O', 'O', 'O' ],
+            [ 'O', 'X', 'O' ]
+        ]
+        self.result = [ [ 1, 3 ], [ 4, 6 ], [ 7, 9 ] ]
+
+    # player = player or target ('O", "X")
+
+    def GetColByIndex(self, position):
+        for index, range_list in enumerate(self.result):
+            start, end = range_list
+            if position >= start and position <= end:
+                return index, position - start
+        return -1, -1
+
+    def SetColByIndex(self, position, player):
+        index, position = self.GetColByIndex(position)
+        if index == -1:
+            print("there's a wrong position, write from 1 to 9")
+            return False
+
+        if self.matrice[ index ][ position ] is not None:
+            print("this position already is busy, choose another one.")
+            return False
+
+        self.matrice[ index ][ position ] = player
+        return True
+
+    # input...
+    # if SetColByIndex(input_position, player):
+    #    print("You succes blabla at position.")
+
+    def IsVertical(self, player):
+        # x[0][0], x[1][0], x[2][0]
+        # x[0][1], x[1][1], x[2][1]
+        # x[0][2], x[1][2], x[2][2]
+
+        vertical_col_1 = self.matrice[ 0 ][ 0 ], self.matrice[ 1 ][ 0 ], self.matrice[ 2 ][ 0 ]
+        vertical_col_2 = self.matrice[ 0 ][ 1 ], self.matrice[ 1 ][ 1 ], self.matrice[ 2 ][ 1 ]
+        vertical_col_3 = self.matrice[ 0 ][ 2 ], self.matrice[ 1 ][ 2 ], self.matrice[ 2 ][ 2 ]
+
+        player_data = tuple([ player for _ in range(3) ])
+
+        if player_data == vertical_col_1 or player_data == vertical_col_2 or player_data == vertical_col_3:
+            return True
+        return False
+
+    def IsHorizontal(self, player):
+        # x[0][0], x[0][1], x[0][2]
+        # x[1][0], x[1][0], x[1][0]
+        # x[2][0], x[2][0], x[2][0]
+
+        horizontal_row_1 = self.matrice[ 0 ][ 0 ], self.matrice[ 0 ][ 1 ], self.matrice[ 0 ][ 2 ]
+        horizontal_row_2 = self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ]
+        horizontal_row_3 = self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ]
+
+        player_data = tuple([ player for _ in range(3) ])
+
+        if player_data == horizontal_row_1 or player_data == horizontal_row_2 or player_data == horizontal_row_3:
+            return True
+        return False
+
+    def IsDiagonal(self, player):
+        # x[0][0], x[1][1], x[2][2]
+        # x[0][2], x[1][1], x[2][0]
+
+        diagonal_1 = self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ]
+        diagonal_2 = self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ]
+
+        player_data = tuple([ player for _ in range(3) ])
+
+        if player_data == diagonal_1 or player_data == diagonal_2:
+            return True
+        return False
+
+    # print(tuple(["O" for i in range(3)]))
+
+
+game = PlayGame()
+print(game.IsHorizontal('O'))
+print(game.IsDiagonal('O'))
+print(game.IsVertical('O'))
+print(game.SetColByIndex(0, 'X'))
