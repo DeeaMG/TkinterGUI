50927419f2fd608b7f7d0906662aff871c2caca6
DeeaMG <deeaganea@gmail.com>
2019-10-30 02:10:23 +0200

    Setting the players
    Setting the player ('X' or 'O'), then depending on the player choice setting the computer.

--- TicTacToeGame/main.py
+++ TicTacToeGame/main.py
@@ -1,14 +1,26 @@
+
+
 class PlayGame:
 
     def __init__(self):
         self.matrice = [
-            [ 'O', 'O', 'O' ],
-            [ 'O', 'O', 'O' ],
-            [ 'O', 'X', 'O' ]
+            [None, None, None],
+            [None, None, None],
+            [None, None, None]
         ]
-        self.result = [ [ 1, 3 ], [ 4, 6 ], [ 7, 9 ] ]
+        self.result = [[1, 3], [4, 6], [7, 9]]
 
-    # player = player or target ('O", "X")
+        self.SetPlayers()
+
+    def SetPlayers(self):
+        self.player = input('Choose witch player you want to be X/O: ')
+        while self.player.upper() not in ['X', 'O']:
+            self.player = input('Choose witch player you want to be X/O: ')
+
+        if self.player == 'X':
+            self.player_target = "O"
+        else:
+            self.player_target = "X"
 
     def GetColByIndex(self, position):
         for index, range_list in enumerate(self.result):
@@ -23,11 +35,11 @@ class PlayGame:
             print("there's a wrong position, write from 1 to 9")
             return False
 
-        if self.matrice[ index ][ position ] is not None:
+        if self.matrice[index][position] is not None:
             print("this position already is busy, choose another one.")
             return False
 
-        self.matrice[ index ][ position ] = player
+        self.matrice[index][position] = player
         return True
 
     # input...
@@ -39,11 +51,11 @@ class PlayGame:
         # x[0][1], x[1][1], x[2][1]
         # x[0][2], x[1][2], x[2][2]
 
-        vertical_col_1 = self.matrice[ 0 ][ 0 ], self.matrice[ 1 ][ 0 ], self.matrice[ 2 ][ 0 ]
-        vertical_col_2 = self.matrice[ 0 ][ 1 ], self.matrice[ 1 ][ 1 ], self.matrice[ 2 ][ 1 ]
-        vertical_col_3 = self.matrice[ 0 ][ 2 ], self.matrice[ 1 ][ 2 ], self.matrice[ 2 ][ 2 ]
+        vertical_col_1 = self.matrice[0][0], self.matrice[1][0], self.matrice[2][0]
+        vertical_col_2 = self.matrice[0][1], self.matrice[1][1], self.matrice[2][1]
+        vertical_col_3 = self.matrice[0][2], self.matrice[1][2], self.matrice[2][2]
 
-        player_data = tuple([ player for _ in range(3) ])
+        player_data = tuple([player for _ in range(3)])
 
         if player_data == vertical_col_1 or player_data == vertical_col_2 or player_data == vertical_col_3:
             return True
@@ -54,11 +66,11 @@ class PlayGame:
         # x[1][0], x[1][0], x[1][0]
         # x[2][0], x[2][0], x[2][0]
 
-        horizontal_row_1 = self.matrice[ 0 ][ 0 ], self.matrice[ 0 ][ 1 ], self.matrice[ 0 ][ 2 ]
-        horizontal_row_2 = self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ]
-        horizontal_row_3 = self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ]
+        horizontal_row_1 = self.matrice[0][0], self.matrice[0][1], self.matrice[0][2]
+        horizontal_row_2 = self.matrice[1][0], self.matrice[1][0], self.matrice[1][0]
+        horizontal_row_3 = self.matrice[2][0], self.matrice[2][0], self.matrice[2][0]
 
-        player_data = tuple([ player for _ in range(3) ])
+        player_data = tuple([player for _ in range(3)])
 
         if player_data == horizontal_row_1 or player_data == horizontal_row_2 or player_data == horizontal_row_3:
             return True
@@ -68,10 +80,10 @@ class PlayGame:
         # x[0][0], x[1][1], x[2][2]
         # x[0][2], x[1][1], x[2][0]
 
-        diagonal_1 = self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ], self.matrice[ 1 ][ 0 ]
-        diagonal_2 = self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ], self.matrice[ 2 ][ 0 ]
+        diagonal_1 = self.matrice[1][0], self.matrice[1][0], self.matrice[1][0]
+        diagonal_2 = self.matrice[2][0], self.matrice[2][0], self.matrice[2][0]
 
-        player_data = tuple([ player for _ in range(3) ])
+        player_data = tuple([player for _ in range(3)])
 
         if player_data == diagonal_1 or player_data == diagonal_2:
             return True
@@ -81,7 +93,3 @@ class PlayGame:
 
 
 game = PlayGame()
-print(game.IsHorizontal('O'))
-print(game.IsDiagonal('O'))
-print(game.IsVertical('O'))
-print(game.SetColByIndex(0, 'X'))
