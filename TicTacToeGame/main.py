94e55fce5c9a2750ad1194fe905ffd635c319389
DeeaMG <deeaganea@gmail.com>
2019-11-03 03:25:34 +0200

    - Made dictionary with strings, - Moved function PlayAgain() in class, - Changed positions of functions.

--- TicTacToeGame/main.py
+++ TicTacToeGame/main.py
@@ -3,45 +3,49 @@ from random import choice
 
 
 class TicTacToeGame:
-    START_END_ROWS_LIST = [[1, 3], [4, 6], [7, 9]]
-    POSSIBLE_POSITIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    START_END_ROWS_LIST: list = [[1, 3], [4, 6], [7, 9]]
+    POSSIBLE_POSITIONS: list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    MAX_ROW = 3
-    MAX_COL = 3
+    MAX_ROW: int = 3
+    MAX_COL: int = 3
+
+    TranslateDict: dict = {
+        'PLAYER_TURN_MSG'         : ["_____________\n", "{} turn.\n{} moves.", "\n_____________"],
+        'COMPUTER_TURN_MSG'       : ["_____________\n", "Is {}'s turn, wait", "\n_____________"],
+        'GET_NAME_MSG'            : 'Enter your name: ',
+        'CHOOSE_PLAYER_MSG'       : 'Choose witch player you want to be X/O: ',
+        'ASK_FOR_POS_MSG'         : 'Choose a position from 1 to 9: ',
+        'PLAY_WITH_FRIEND_MSG'    : 'Do you want to play with a friend? (YES/NO) ',
+        'PLAY_AGAIN_MSG'          : 'Do you want to play again? (YES/NO) ',
+        'GOODBYE_MSG'             : 'Thank you for playing this game.',
+        'POS_ERR_MSG_LIST'        : ['This is a wrong position, please choose one from 1 to 9',
+                                     'This position is busy, choose another one.'
+                                     ]
+    }
 
     def __init__(self):
-        self.player1 = ''
-        self.player2 = ''
-        self.playerComputer = ''
+        self.player1: str        = ''
+        self.player2: str        = ''
+        self.playerComputer: str = ''
+        self.player1Name: str    = ''
+        self.player2Name: str    = ''
 
-        self.game_still_going = True
-        self.mainArray = [
+        self.game_still_going: bool = True
+        self.mainArray: list = [
             [None, None, None],
             [None, None, None],
             [None, None, None]
         ]
 
-        self.displayedGrid = [
+        self.displayedGrid: list = [
             ['-|', '-|', '-', '\t\t1|2|3'],
             ['-|', '-|', '-', '\t\t4|5|6'],
             ['-|', '-|', '-', '\t\t7|8|9']
         ]
-        self.SetPlayers()
-
-    @staticmethod
-    def PlayAgain():
-        answer = input('Do you want to play again? (YES/NO) ')
-        if answer[0].upper() == 'Y':
-            playAgain = TicTacToeGame()
-            playAgain.PlayGame()
-        elif answer[0].upper() != 'Y':
-            print('Thank you for playing this game.')
-
-    @staticmethod
-    def GetPlayerDataList(player):
-        return [player] * 3
 
     def PlayGame(self):
+        self.SetPlayers()
+
         while self.game_still_going:
             self.GridDisplay()
             self.ShowTurn()
@@ -49,18 +53,11 @@ class TicTacToeGame:
             self.CheckWinner()
             self.SwitchTurn()
 
-    def SetSecondPlayer(self):
-        question = input('Do you want to play with a friend? (YES/NO) ')
-
-        if question[0].upper() == 'Y':
-            self.player2 = self.secondPlayer
-        else:
-            self.playerComputer = self.secondPlayer
-
     def SetPlayers(self):
-        self.player1 = input('Choose witch player you want to be X/O: ').upper()
+        self.player1Name = input(self.TranslateDict[GET_NAME_MSG])
+        self.player1 = input(self.TranslateDict[CHOOSE_PLAYER_MSG]).upper()
         while self.player1 not in ['X', 'O']:
-            self.player1 = input('Choose witch player you want to be X/O: ').upper()
+            self.player1 = input(self.TranslateDict[CHOOSE_PLAYER_MSG]).upper()
 
         if self.player1 == 'X':
             self.secondPlayer = 'O'
@@ -71,7 +68,14 @@ class TicTacToeGame:
 
         self.SetSecondPlayer()
 
-        print(self.player1, self.player2, self.playerComputer)
+    def SetSecondPlayer(self):
+        question = input(self.TranslateDict[PLAY_WITH_FRIEND_MSG])
+
+        if question[0].upper() == 'Y':
+            self.player2Name = input(self.TranslateDict[GET_NAME_MSG])
+            self.player2 = self.secondPlayer
+        else:
+            self.playerComputer = self.secondPlayer
 
     def GridDisplay(self, newLine='\n'):
         result = ''
@@ -81,42 +85,60 @@ class TicTacToeGame:
             result += newLine
         print(result)
 
-    def SwitchTurn(self):
-        if self.currentPlayer == self.player1:
-            self.currentPlayer = self.secondPlayer
-        else:
-            self.currentPlayer = self.player1
-
     def ShowTurn(self):
         if self.currentPlayer == self.player1 or self.currentPlayer == self.player2:
-            print(
-                "_____________\n" +
-                "Is {} turn.".format(self.currentPlayer) +
-                "\n_____________"
-            )
+            print(self.TranslateDict[PLAYER_TURN_MSG][0] +
+                  self.TranslateDict[PLAYER_TURN_MSG][1].format(self.GetName(), self.currentPlayer) +
+                  self.TranslateDict[PLAYER_TURN_MSG][2]
+                  )
         elif self.currentPlayer == self.playerComputer:
-            result = "_____________\n" + "Is {}'s turn, wait".format(self.currentPlayer)
+            result = self.TranslateDict[COMPUTER_TURN_MSG][0] + self.TranslateDict[COMPUTER_TURN_MSG][1].format(self.currentPlayer)
             print(result, end='')
             for i in range(3):
                 if i == 0:
                     sleep(1)
                 print(".", end='')
                 sleep(1)
-            print("\n_____________")
+            print(self.TranslateDict[COMPUTER_TURN_MSG][2])
+
+    def LoadPosition(self):
+        if self.currentPlayer == self.player1 or self.currentPlayer == self.player2:
+            position = input(self.TranslateDict[ASK_FOR_POS_MSG])
+            if position.isdigit():
+                pos = int(position)
+                if pos in self.POSSIBLE_POSITIONS:
+                    self.SetColByIndex(pos, self.currentPlayer)
+            else:
+                self.LoadPosition()
+        else:
+            self.SetColByIndex(self.GetComputerPos(), self.currentPlayer)
 
     def CheckWinner(self):
-        if self.CheckIfWinOnRows(self.currentPlayer) or self.CheckIfWinOnCols(self.currentPlayer) or self.CheckIfWinOnDiagonals(
-                self.currentPlayer):
+        if self.CheckIfWinOnRows(self.currentPlayer) or self.CheckIfWinOnCols(self.currentPlayer) or self.CheckIfWinOnDiagonals(self.currentPlayer):
             self.GridDisplay()
-            print("{} won.".format(self.currentPlayer))
+            print("{} won.".format(self.GetName()))
             self.game_still_going = False
             self.PlayAgain()
+        elif self.CheckIfTie():
+            self.GridDisplay()
+            print('Is tie.')
+            self.game_still_going = False
+            self.PlayAgain()
+
+    def SwitchTurn(self):
+        if self.currentPlayer == self.player1:
+            self.currentPlayer = self.secondPlayer
         else:
-            if self.CheckIfTie():
-                self.GridDisplay()
-                print('Is tie.')
-                self.game_still_going = False
-                self.PlayAgain()
+            self.currentPlayer = self.player1
+
+    def PlayAgain(self):
+        answer = input(self.TranslateDict[PLAY_AGAIN_MSG])
+        if answer[0].upper() == 'Y':
+            print('\n\n\n')
+            playAgain = TicTacToeGame()
+            playAgain.PlayGame()
+        elif answer[0].upper() != 'Y':
+            print(self.TranslateDict[GOODBYE_MSG])
 
     # Setting both in the mainArray and in the diplayedGrid the player, on the position he chose.
     def SetColByIndex(self, position, player):
@@ -125,11 +147,11 @@ class TicTacToeGame:
 
         # Ask's again for position.
         if (player == self.player1 or player == self.player2) and index == -1:
-            print("This is a wrong position, please choose one from 1 to 9")
+            print(self.TranslateDict[POS_ERR_MSG_LIST][0])
             self.LoadPosition()
 
         elif (player == self.player1 or player == self.player2) and self.mainArray[index][position]:
-            print("This position is busy, choose another one.")
+            print(self.TranslateDict[POS_ERR_MSG_LIST][1])
             self.LoadPosition()
 
         # Changes elements from mainArray and displayedGrid (from position [index][position]) with the current player.
@@ -140,18 +162,13 @@ class TicTacToeGame:
             curString = curString.replace(curString[0], player)
             self.displayedGrid[index][position] = curString
 
-    # Get position from the current player(either player1 or playerComputer).
-    def LoadPosition(self):
-        if self.currentPlayer == self.player1 or self.currentPlayer == self.player2:
-            position = input('Choose a position from 1 to 9: ')
-            if position.isdigit():
-                pos = int(position)
-                if pos in self.POSSIBLE_POSITIONS:
-                    self.SetColByIndex(pos, self.currentPlayer)
-            else:
-                self.LoadPosition()
+    def GetName(self):
+        if self.currentPlayer == self.player1:
+            return self.player1Name
+        elif self.player2Name:
+            return self.player2Name
         else:
-            self.SetColByIndex(self.GetComputerPos(), self.currentPlayer)
+            return self.currentPlayer
 
     # Get's the row and the column from the position the player chose.
     def GetColByIndex(self, position):
@@ -175,25 +192,6 @@ class TicTacToeGame:
 
         return position
 
-    def CheckIfWinOnCols(self, player):
-        checkColsList = []
-        for col in range(self.MAX_COL):
-            checkColsList.append([self.mainArray[row][col] for row in range(self.MAX_ROW)])
-        return self.GetPlayerDataList(player) in checkColsList
-
-    def CheckIfWinOnRows(self, player):
-        checkRowsList = []
-        for row in range(self.MAX_ROW):
-            checkRowsList.append([self.mainArray[row][col] for col in range(self.MAX_COL)])
-        return self.GetPlayerDataList(player) in checkRowsList
-
-    def CheckIfWinOnDiagonals(self, player):
-        diagonal_1 = [self.mainArray[i][i] for i in range(3)]
-        diagonal_2 = [self.mainArray[i][2 - i] for i in range(3)]
-
-        player_data = self.GetPlayerDataList(player)
-        return player_data == diagonal_1 or player_data == diagonal_2
-
     def CheckIfTie(self):
         count = 0
         for row in self.mainArray:
@@ -201,6 +199,28 @@ class TicTacToeGame:
                 if col:
                     count += 1
         return count == self.MAX_COL * self.MAX_ROW
+
+    def CheckIfWinOnCols(self, player):
+        checkColsList = []
+        for col in range(self.MAX_COL):
+            checkColsList.append([self.mainArray[row][col] for row in range(self.MAX_ROW)])
+        return GetPlayerDataList(player) in checkColsList
+
+    def CheckIfWinOnRows(self, player):
+        checkRowsList = []
+        for row in range(self.MAX_ROW):
+            checkRowsList.append([self.mainArray[row][col] for col in range(self.MAX_COL)])
+        return GetPlayerDataList(player) in checkRowsList
+
+    def CheckIfWinOnDiagonals(self, player):
+        diagonal_1 = [self.mainArray[i][i] for i in range(3)]
+        diagonal_2 = [self.mainArray[i][2 - i] for i in range(3)]
+        player_data = GetPlayerDataList(player)
+        return player_data == diagonal_1 or player_data == diagonal_2
+
+
+def GetPlayerDataList(player):
+    return [player] * 3
 
 
 game = TicTacToeGame()
