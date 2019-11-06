9e3ed0ed91ad3eb47658cacd0d459d629cd4e603
DeeaMG <deeaganea@gmail.com>
2019-11-07 01:34:50 +0200

    - Created function to play again. - Already there are two windows(the game window and the one where the player has to choose to be X or O and to play with a friend or with the computer.)

--- TicTacToeGame/mainGUI.py
+++ TicTacToeGame/mainGUI.py
@@ -2,22 +2,27 @@ from tkinter import *
 
 
 class GameWindow:
-
     MAX_ROW: int = 3
     MAX_COL: int = 3
 
     def __init__(self):
-        self.gameWnd = Tk()
-        self.gameWnd.title('X and O Game')
-        self.gameWnd.configure(background='light blue')
-        self.gameWnd.geometry('300x300')
-        self.onClickText = StringVar()
-        self.player1 = 'X'
-        self.player2 = 'O'
-        self.game_still_going = True
-        self.currentPlayer = self.player1
-        self.onClick = False
+        self.gameWnd = None
 
+        self.buttList = []
+        self.player1 = ''
+        self.player2 = ''
+        self.playerComputer = ''
+        self.currentPlayer = ''
+        self.onClick = False
+        self.game_still_going = True
+
+       # self.GetPlayers()
+
+    def PlayGame(self, button):
+        self.SetButtTxtOnClick(button)
+        self.SwitchTurn()
+
+    def SetGrid(self):
         canvas = Canvas(self.gameWnd, width=300, height=300, highlightthickness=0, bg="PaleGreen1")
         canvas.pack()
 
@@ -64,11 +69,34 @@ class GameWindow:
             [self.buttonPos7, self.buttonPos8, self.buttonPos9]
         ]
 
-        self.gameWnd.mainloop()
+    def SetFirstPlayer(self, text):
+        self.OnClickFlag()
+        if self.onClick:
+            self.player1 = text
+            print('player1 is : %s' % self.player1)
 
-    def PlayGame(self, button):
-        self.SetButtTxtOnClick(button)
-        self.SwitchTurn()
+        self.SetPlayers()
+
+    def SetSecondPlayer(self, answer):
+        self.gameWnd = Tk()
+        self.gameWnd.title('X and O Game')
+        self.gameWnd.configure(background='light blue')
+        self.gameWnd.geometry('300x300')
+
+        if answer[0].upper() == 'Y':
+            self.player2 = self.secondPlayer
+        elif answer[0].upper() == 'N':
+            self.playerComputer = self.secondPlayer
+        print('player2 is : %s\nplayerComputer is: %s' % (self.player2, self.playerComputer))
+        self.SetGrid()
+
+    def SetPlayers(self):
+        if self.player1 == 'X':
+            self.secondPlayer = 'O'
+            self.currentPlayer = self.player1
+        elif self.player1 == 'O':
+            self.secondPlayer = 'X'
+            self.currentPlayer = self.secondPlayer
 
     def OnClickFlag(self):
         if not self.onClick:
@@ -85,7 +113,7 @@ class GameWindow:
 
     def SwitchTurn(self):
         if self.currentPlayer == self.player1:
-            self.currentPlayer = self.player2
+            self.currentPlayer = self.secondPlayer
         else:
             self.currentPlayer = self.player1
 
@@ -93,10 +121,12 @@ class GameWindow:
         if self.CheckWinOnCols() or self.CheckWinOnRows() or self.CheckWinOnDiagonals():
             print('Yeeey')
             self.gameWnd.destroy()
+            PlayAgain(self.currentPlayer, self)
 
         elif self.IsTie():
             print('TIE')
             self.gameWnd.destroy()
+            PlayAgain(None, self)
 
     def IsTie(self):
         count = 0
@@ -124,7 +154,110 @@ class GameWindow:
         player_data = GetPlayerDataList(self.currentPlayer)
         return player_data == diagonal_1 or player_data == diagonal_2
 
+
 def GetPlayerDataList(player):
     return [player] * 3
 
+
+class PlayAgain:
+    def __init__(self, currentPlayer, wndGame):
+        self.wndGame = wndGame
+        self.currentPlayer = currentPlayer
+        self.exitWnd = Tk()
+        self.exitWnd.title('Play again.')
+        self.exitWnd.configure(background='light blue')
+        self.exitWnd.geometry('300x200')
+
+        self.onclick = False
+
+        if self.currentPlayer:
+            self.WinnerMsg()
+        else:
+            self.TieMsg()
+
+        self.PlayAgainMsg()
+
+        self.exitWnd.mainloop()
+
+    def TieMsg(self):
+        tieMsg = Label(self.exitWnd, text='Is a tie.', font=('Tahoma', 40), background='light blue')
+        tieMsg.place(x=65, y=15)
+
+    def WinnerMsg(self):
+        congratLabel = Label(self.exitWnd, text='Congratulations!', font=('Tahoma', 25), background='light blue')
+        winLabel = Label(self.exitWnd, text='{} won.'.format(self.currentPlayer), font=('Tahoma', 20), background='light blue')
+
+        congratLabel.place(x=30, y=5)
+        winLabel.place(x=117, y=50)
+
+    def PlayAgainMsg(self):
+        msgLabel = Label(self.exitWnd, text='Do you want to play again?', font=('Tahoma', 17), background='light blue')
+        yesButton = Button(self.exitWnd, padx=30, pady=10, bd=1, bg='PaleGreen1', text='Yes', activebackground='PaleGreen2',
+                           command=lambda: self.GetResponse(yesButton))
+        noButton = Button(self.exitWnd, padx=30, pady=10, bd=1, bg='PaleGreen1', text='No', activebackground='PaleGreen2',
+                          command=lambda: self.GetResponse(noButton))
+
+        msgLabel.place(x=10, y=95)
+        yesButton.place(x=50, y=140)
+        noButton.place(x=160, y=140)
+
+    def OnClickFlag(self):
+        if not self.onclick:
+            self.onclick = True
+
+    def GetResponse(self, button):
+        self.OnClickFlag()
+        if self.onclick:
+            if button.cget('text') == 'Yes':
+                self.exitWnd.destroy()
+                SelectPlayer(self.wndGame)
+            elif button.cget('text') == 'No':
+                self.exitWnd.destroy()
+
 game = GameWindow()
+
+class SelectPlayer:
+    def __init__(self, parent):
+        self.parent = parent
+
+        self.window = Tk()
+        self.window.title('X and O Game')
+        self.window.configure(background='light blue')
+        self.window.geometry('300x100')
+
+        self.choosePlayerLabel = Label(self.window, text='Which player do you want to be:', font=('Tahoma', 15),
+                                       background='light blue')
+        self.XButton = Button(self.window, padx=25, pady=5, bd=1, bg='PaleGreen1', text='X', activebackground='PaleGreen2',
+                              command=lambda: self.SetFirstPlayer(self.XButton.cget('text')))
+        self.OButton = Button(self.window, padx=25, pady=5, bd=1, bg='PaleGreen1', text='O', activebackground='PaleGreen2',
+                              command=lambda: self.SetFirstPlayer(self.OButton.cget('text')))
+        self.playWithFriendLabel = Label(self.window, text='Do you want to play with a friend?', font=('Tahoma', 14),
+                                         background='light blue')
+        self.yesButton = Button(self.window, padx=20, pady=5, bd=1, bg='PaleGreen1', text='Yes', activebackground='PaleGreen2',
+                                command=lambda: self.SetSecondPlayer(self.yesButton.cget('text')))
+        self.noButton = Button(self.window, padx=20, pady=5, bd=1, bg='PaleGreen1', text='No', activebackground='PaleGreen2',
+                               command=lambda: self.SetSecondPlayer(self.noButton.cget('text')))
+
+        self.choosePlayerLabel.place(x=2, y=5)
+        self.XButton.place(x=70, y=35)
+        self.OButton.place(x=160, y=35)
+
+        self.window.mainloop()
+
+    def SetFirstPlayer(self, text):
+        self.parent.SetFirstPlayer(text)
+
+        self.choosePlayerLabel.destroy()
+        self.XButton.destroy()
+        self.OButton.destroy()
+
+        self.playWithFriendLabel.place(x=2, y=5)
+        self.yesButton.place(x=70, y=35)
+        self.noButton.place(x=160, y=35)
+
+    def SetSecondPlayer(self, answer):
+        self.parent.SetSecondPlayer(answer)
+        self.window.destroy()
+
+
+select_player = SelectPlayer(game)
